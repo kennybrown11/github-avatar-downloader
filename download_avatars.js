5ab26efce0f9c29bf76592e041b45830d0f894f5
Kenny <kennybrown11@hotmail.com>
2018-07-10 01:46:04 +0000

    added authorization token

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,21 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+var options = { 
+  url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+  headers: {
+    'User-Agent': 'request',
+    "Authorization" : "2d21989c3dd345a26a53a7b96258108bf4ce9b98"
+  }
+};
+  request(options, function(err, res, body){
+  cb(err, body)
+})
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
