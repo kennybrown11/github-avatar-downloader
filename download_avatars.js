4617e0abc1e00825d7fc55a2a1d0bc6bb237d25d
Kenny <kennybrown11@hotmail.com>
2018-07-10 03:30:55 +0000

    Implement download function

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs')
 
 //console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -8,18 +9,31 @@ var options = {
   headers: {
     'User-Agent': 'request',
   }
-};
-cb(err, body);  
+}; 
 
 request(options, function(err, res, body){
 var results = JSON.parse(body);
   results.forEach(function(showURL) {
-  console.log("Avatar URL " + showURL.avatar_url);
+  //console.log("Avatar URL " + showURL.avatar_url);
   }) 
+  cb(err, body);  
 })
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+  .on('error', function (err) {  
+    throw err; 
+  })
+  .on('response', function (response) {
+    console.log('Response Status Code: ', response.statusCode);
+  })
+  .pipe(fs.createWriteStream(filePath)); 
+}
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   //console.log("Errors:", err);
   //console.log("Result:", result);
 });
+
